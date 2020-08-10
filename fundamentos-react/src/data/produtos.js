9fda2b4d20315389705df98a5e3d35c6045faaaf
Rodrigo <rodrigobergamindev@gmail.com>
2020-08-10 16:56:03 -0300

    React Fundamentos

--- fundamentos-react/src/data/produtos.js
+++ fundamentos-react/src/data/produtos.js
@@ -1,11 +1,9 @@
 export default [
     {id:1, nome: 'Smart TV - 50 polegadas', preco: 10000.00},
     {id:2, nome: 'iPhone XS', preco: 5.700},
-    {id:3, nome: 'Jefferson', preco: 8.300},
-    {id:4, nome: 'Douglas', preco: 9.500},
-    {id:5, nome: 'Anabelle', preco: 3.850},
-    {id:6, nome: 'Keyla', preco: 5.550},
-    {id:7, nome: 'Camille', preco: 6.650},
-    {id:8, nome: 'Lin', preco: 7.550},
-    {id:9, nome: 'Bren', preco: 10.550}
+    {id:3, nome: 'Xiaomi MI 9', preco: 8.300},
+    {id:4, nome: 'Jogo de Facas Tramontina', preco: 250},
+    {id:5, nome: 'Airfryer', preco: 999},
+    {id:6, nome: 'Aspirador', preco: 120},
+    {id:7, nome: 'Camiseta LO', preco: 59.5},
 ]
